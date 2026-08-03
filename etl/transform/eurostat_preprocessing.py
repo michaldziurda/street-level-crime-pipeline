040f39799e92a7640e2b2f91ae97a07d3ed388bb
michaldziurda <dziurdamichal@gmail.com>
2026-08-03 10:47:39 +0200

    Quick bugfixes in preprocessing

--- etl/transform/eurostat_preprocessing.py
+++ etl/transform/eurostat_preprocessing.py
@@ -28,6 +28,8 @@ def preprocess_eurostat_data(config: Config):
             
             # Initial file contains consecutive years as seaparate column. Reshaping df to have years as a single column
             df = pd.melt(df, id_vars=['iccs', 'unit', 'geo'], value_vars=df.filter(regex="[0-9]{4}").columns, var_name='year', value_name='crime_count')
+            df['year'] = df['year'].astype(int)
+            df['crime_count'] = pd.to_numeric(df['crime_count'], errors='coerce')
 
             # Assign an id for unique tracking
             df['id'] = df[['iccs', 'unit', 'geo', 'year']].apply(lambda row: "_".join(row), axis=1)
